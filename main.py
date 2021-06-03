2e747cee60e7bcf3935a1eeeb5bb4ad89ae856e8
Walid Abdelal <walidmabdelal@gmail.com>
2021-06-03 20:39:01 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is ..let's see
+import pandas as pd
+import warnings
+import datetime as dt
+import pandas_datareader.data as pd_reader
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.dates as mdates
+import plotly.express as px
+
+
+
+#warnings.filterwarnings('ignore')
+pd.core.common.is_list_like = pd.api.types.is_list_like
+
+
+
+# Main code
+if __name__ == '__main__':
+
+
